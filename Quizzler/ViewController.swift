3809333b48be2722d91d3df20aaa4f348ec68834
Anthony Ruiz <yobakaj@gmail.com>
2018-04-30 21:23:44 -0400

    [update] functionality for next question and show alert when quiz has been finished

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestion = QuestionBank()
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,12 +21,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+      
+      questionLabel.text = allQuestion.list.first?.questionTex
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+      var pickedAnswer : Bool = false
+      if sender.tag == 1 {
+         pickedAnswer = true
+      }else if sender.tag == 2 {
+         pickedAnswer = false
+      }
+      
+      checkAnswer(answer: pickedAnswer)
+      questionNumber = questionNumber + 1
+      nextQuestion()
     }
     
     
@@ -35,16 +46,37 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+      
+      if questionNumber <= allQuestion.list.count - 1{
+         print(allQuestion.list.count - 1)
+         questionLabel.text = allQuestion.list[questionNumber].questionTex
+      }else{
+         print("final de las preguntas")
+      }
     }
     
     
-    func checkAnswer() {
-        
+   func checkAnswer(answer: Bool) {
+      
+      let correctAnswer = allQuestion.list[questionNumber].answer
+      if correctAnswer == answer {
+         print("respuesta correcta")
+      }else{
+         let alert = UIAlertController(title: "Awesome", message: "Do you Want To Play Again?", preferredStyle: .alert)
+         
+         let restarQuizz = UIAlertAction(title: "Restart", style: .default, handler: {(UIAlertAction) in
+            
+            self.startOver()
+         })
+         alert.addAction(restarQuizz)
+         present(alert, animated: true, completion: nil)
+      }
     }
     
     
     func startOver() {
+      questionNumber = 0
+      nextQuestion()
        
     }
     
